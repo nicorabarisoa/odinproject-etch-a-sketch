3a0f003b92473577410cdf426c9ace39e1883bd6
Nico <nicorabarisoa@gmail.com>
2024-07-25 15:45:24 +0200

    ajout bouton, et grille interactive

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 let container = document.querySelector("#container");
+let body = document.querySelector("body");
+let numberSquare = 256;
+let button =  document.createElement("button");
+
+button.textContent="Paramétrer le nombre de case"
+
+// Ajouter le bouton avant le conteneur
+body.insertBefore(button,container);
 
 
-for (let i = 0; i<=256;i++){
+for (let i = 0; i<numberSquare;i++){
    let square = document.createElement("div");
+   square.classList.add("square")
    container.appendChild(square);
 
 }
+
+let squares = document.querySelectorAll(".square");
+
+squares.forEach((square) =>{
+
+square.addEventListener("mouseover", (e) => {
+    e.target.style.background ="red";
+})
+
+
+})
